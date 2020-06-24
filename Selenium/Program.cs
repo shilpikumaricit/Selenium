3939b1823a2f4ffb84511b99fe82de893dbb3c4a
srisingh <v-srisingh@expedia.com>
2020-06-24 11:21:08 +0800

    Selenium code to open chrome and type google.com

---
+++ Selenium/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            //Find the element
+            IWebElement webElement = driver.FindElement(By.Name("q"));
+            //perform operation
+            webElement.SendKeys("Execute Automation");
+            driver.Close();
+        }
+    }
+}
